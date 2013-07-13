ade0c21bbe75d6e05e675467c15b008e39994131
Ubuntu <ubuntu@ip-172-31-12-222.us-west-2.compute.internal>
2013-07-13 17:18:40 +0000

    Homework 1 - final

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require(fs);
+var buf = fs.readFileSync('index.html','utf-8');
+var string = buf.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
